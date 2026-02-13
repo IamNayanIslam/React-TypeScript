f70bb31070eb11180e8ca94c5b24ad143a80536f
Nayan Islam <iamnayanislam@gmail.com>
2026-02-13 14:22:30 +0600

    array and object types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import User from "./Components/User";
+
 function App() {
+  const user1 = {
+    name: "Nayan Islam",
+    age: 28,
+    isActive: true,
+    langs: ["Bengali", "English", "Hindi"],
+  };
+
+  const user2 = {
+    name: "Hiya Akter",
+    age: 23,
+    isActive: false,
+    langs: ["Bengali", "English"],
+  };
+
   return (
     <>
       <div>
-        <h2 className="text-center my-4 text-2xl font-bold">
-          React TypeScript
-        </h2>
+        <h1 className="text-center my-4 text-2xl font-bold">User Management</h1>
+        <div className="flex flex-col items-center">
+          <User user={user1} />
+          <User user={user2} />
+        </div>
+        {/* <User
+          name="Hiya Akter"
+          age={23}
+          isActive={false}
+          langs={["Bengali", "English"]}
+        /> */}
       </div>
     </>
   );
---
+++ src/Components/User.tsx
@@ -0,0 +1,27 @@
+type UserProps = {
+  user: {
+    name: string;
+    age: number;
+    isActive: boolean;
+    langs: string[];
+  };
+};
+
+const User = ({ user }: UserProps) => {
+  return (
+    <div className="border border-gray-600 rounded-md mt-4 w-1/4 py-4">
+      <h2 className="text-center font-bold text-xl">{user.name}</h2>
+      <p className="text-center">
+        <span
+          className={`inline-block border border-transparent h-[10px] w-[10px] rounded-full ${user.isActive ? "bg-green-600" : "bg-red-600"} `}
+        ></span>{" "}
+        {user.name} is {user.age} years old.
+      </p>
+      <p className="text-center">
+        Speaks: {user.langs && <span>{user.langs.join(", ")} </span>}
+      </p>
+    </div>
+  );
+};
+
+export default User;
